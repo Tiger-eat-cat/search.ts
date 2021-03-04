e9d805b3baa993dd9d60edb43dca26194ff127dc
perry.shi <perry.shi@yunlsp.com>
2021-03-04 11:08:30 +0800

    update README.md

--- test/core.spec.ts
+++ test/core.spec.ts
@@ -10,13 +10,13 @@ describe('build search test', () => {
         expect(buildSearch('abc').search(content)).toEqual([{ start: 0, end: 2 }])
     })
     it('test formatter func', () => {
-        const formatter: Formatter = (result, word, start, end) => {
+        const formatter: Formatter = (result, word) => {
             result.push({ name: word })
         }
         const formatter2: Formatter = (result, word, start, end) => {
             result.push({ start, end, name: word })
         }
-        expect(buildSearch(['abc', 'bac']).search(content, formatter)).toEqual([ { name: 'abc' }, { name: 'bac' } ])
+        expect(buildSearch(['abc', 'bc', 'bac']).search(content, formatter)).toEqual([ { name: 'abc' }, { name: 'bc' }, { name: 'bac' } ])
         expect(buildSearch('abc').search(content, formatter2)).toEqual([{ start: 0, end: 2, name: 'abc' }])
     })
 })
